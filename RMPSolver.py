79490e3a03fc878664226b7c59189a573864789e
mfiratttue <m.firat@tue.nl>
2018-04-19 11:49:38 +0200

    myfirstPythoncode

--- RMPSolver.py
+++ RMPSolver.py
@@ -18,3 +18,22 @@ def solveRMP(prob):
 def add_column(prob,inputdepth,segments_set,segment_to_add,leaf):
     
     return add_variable_to_master_and_rebuild(prob,inputdepth,segments_set,segment_to_add,leaf)
+
+def display_RMP_solution(depth,prob):
+    from learn_tree_funcs import get_num_features,get_left_leafs
+    
+    num_leafs = 2**depth 
+    
+    num_nodes = num_leafs-1
+    
+    num_features = get_num_features()
+  
+    for i in range(num_features): #constraint (15), indicator 0
+
+        for j in range(num_nodes):
+            
+            left_leaves = get_left_leafs(j, num_nodes)
+            
+            for l in left_leaves:
+
+                print("Pi("+i+"_"+j+"_"+l+")*= "+"%.2f" % round(prob.solution.get_dual_values(i*len(num_nodes)+j*len(left_leaves)+l),2))
