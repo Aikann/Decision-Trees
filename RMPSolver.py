e9dcee3f3b809126263c99b67221a37806a1d3ba
mfiratttue <m.firat@tue.nl>
2018-04-19 15:10:18 +0200

    RMPDualsDisplay-Done

--- RMPSolver.py
+++ RMPSolver.py
@@ -19,16 +19,23 @@ def add_column(prob,inputdepth,segments_set,segment_to_add,leaf):
     
     return add_variable_to_master_and_rebuild(prob,inputdepth,segments_set,segment_to_add,leaf)
 
-def display_RMP_solution(depth,prob):
-    from learn_tree_funcs import get_num_features,get_left_leafs
+def display_RMP_solution(depth,prob,CGiter):
+    
+    from learn_tree_funcs import get_num_features,get_left_leafs,get_right_leafs,get_data_size
+    from cplex_problems_CG import constraint_indicators
+    
+    
+    print("--------------------- RMP("+CGiter+") Solution ---------------------");
     
     num_leafs = 2**depth 
     
     num_nodes = num_leafs-1
     
     num_features = get_num_features()
+    
+    start_index = constraint_indicators(0)
   
-    for i in range(num_features): #constraint (15), indicator 0
+    for i in range(num_features): #constraint (15),
 
         for j in range(num_nodes):
             
@@ -36,4 +43,43 @@ def display_RMP_solution(depth,prob):
             
             for l in left_leaves:
 
-                print("Pi("+i+"_"+j+"_"+l+")*= "+"%.2f" % round(prob.solution.get_dual_values(i*len(num_nodes)+j*len(left_leaves)+l),2))
+                print("Pi("+i+"_"+j+"_"+l+"-15-)*= "+"%.2f" % round(prob.solution.get_dual_values(start_index+i*len(num_nodes)+j*len(left_leaves)+l),2))
+
+    start_index = constraint_indicators(1)
+
+    for i in range(num_features): #constraint (16),
+
+        for j in range(num_nodes):
+            
+            right_leaves = get_right_leafs(j, num_nodes)
+            
+            for l in right_leaves:
+
+                print("Pi("+i+"_"+j+"_"+l+"-16-)*= "+"%.2f" % round(prob.solution.get_dual_values(start_index+i*len(num_nodes)+j*len(right_leaves)+l),2))
+ 
+    
+    start_index = constraint_indicators(2)
+    data_size = get_data_size()
+    
+    for r in range(data_size): #constraint (17),
+        
+            print("Th_"+r+"*= "+"%.2f" % round(prob.solution.get_dual_values(start_index+r),2))
+    
+    
+    start_index = constraint_indicators(3)
+    
+    for l in range(num_leafs): #constraint (18),
+        
+         print("Be_"+l+"*= "+"%.2f" % round(prob.solution.get_dual_values(start_index+l),2))
+         
+         
+    start_index = constraint_indicators(4)
+     
+    for r in range(data_size): #constraint (19),
+        
+        for l in range(num_leafs):
+    
+            print("Gm_"+r+"_"+l+"*= "+"%.2f" % round(prob.solution.get_dual_values(start_index+r*len(num_leafs)+l),2))
+            
+            
+    print("--------------------- RMP("+CGiter+") Solution ---------------------");
