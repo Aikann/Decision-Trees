bb93fb5a221be0a19797a6cefa8be51779290763
mfiratttue <m.firat@tue.nl>
2018-04-19 13:42:40 +0200

    filesadded

---
+++ BaP_Node.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 13:44:53 2018
+
+@author: Guillaume
+"""
+
+from RMPSolver import add_column, solveRMP, create_new_master
+from nodes_external_management import give_solution_type, check_unicity, adapt_segments_set, hash_seg
+
+from PricingSolver import solve_pricing
+import time
+import matplotlib.pyplot as plt
+import copy
+
+
+def obtain_depth2(d):
+    
+    global depth
+    depth=d
+
+
+class BaP_Node:
+    
+    def __init__(self,segments_set,prob,ID,parent,branched_rows,branched_leaves,H):
+        
+        self.prob=prob #cplex problem
+        self.segments_set=segments_set #list containing lists of sets for each leaf
+        self.ID = ID
+        self.parent = parent
+        self.branched_rows = branched_rows #list of branched rows
+        self.branched_leaves = branched_leaves #list of corresponding leaves
+        self.H = H #hash table for segments
+        
+    def explore(self): #do CG until the master problem is solved
+        
+        plt.figure()
+                
+        plt.show()
+        
+        solveRMP(self.prob)
+        
+        print(self.segments_set)
+                
+        convergence = False
+        
+        global count_iter
+        
+        count_iter=1
+        
+        previous_solution = float('+inf')
+        
+        not_imp=0
+        
+        pricing_method=2
+        
+        while not convergence:
+                        
+            self.prob.solve()
+            
+            #print(self.prob.solution.get_values())
+                                    
+            if count_iter%50==0:
+                
+                pricing_method=1
+                
+            else:
+                
+                #pricing_method=2
+                
+                if previous_solution-0.01<=self.prob.solution.get_objective_value()<=previous_solution+0.01:
+                    
+                    not_imp += 1
+                    
+                if not_imp>20 and pricing_method!=3:
+                    
+                    not_imp, pricing_method = 0, 3
+                    
+                elif not_imp>15 and pricing_method!=2:
+                    
+                    not_imp, pricing_method = 0, 2
+                    
+                #pricing_method = 3 - int(count_iter<52)
+                    
+            previous_solution = self.prob.solution.get_objective_value()
+                        
+            segments_to_be_added, convergence = solve_pricing(depth,self.prob,self.segments_set,self.branched_rows,self.branched_leaves,self.ID,pricing_method)
+                        
+            plt.scatter(count_iter,self.prob.solution.get_objective_value(),color='g')
+            
+            plt.pause(0.01)
+            
+            if count_iter%30==0:
+            
+                print("Current solution value "+str(self.prob.solution.get_objective_value()))
+            
+                print("Number of segments "+str(sum([len(self.segments_set[l]) for l in range(len(self.segments_set))])))
+                
+                #check_unicity(self.segments_set)
+                                
+                #input()
+                                
+                time.sleep(0.1)
+                
+            a=time.time()
+            
+            previous_seg_set = copy.deepcopy(self.segments_set)
+            
+            self.add_segments(segments_to_be_added,True)
+            
+            if not convergence:
+                                                    
+                self.prob = add_column(self.prob,depth,previous_seg_set,segments_to_be_added,self.segments_set)
+                
+            print(count_iter,time.time()-a)
+            
+            """
+            
+            a=time.time()
+                
+            if not convergence:
+                
+                self.prob = construct_master_problem(depth,self.segments_set)
+                
+            print(count_iter,time.time()-a)
+             """     
+            count_iter=count_iter+1
+            
+            
+        
+        self.solution_value = self.prob.solution.get_objective_value()
+        self.solution = self.prob.solution.get_values()
+        self.solution_type = give_solution_type(self.prob)
+        
+    def solve_warp_up(self): #TODO ; return a tuple is_truly_infeasible, segments so that the master problem can be feasible
+        
+        return
+        
+    def add_segments(self,segs_to_add,safe_insertion=False):
+                
+        for l in range(len(self.segments_set)):
+            
+            if segs_to_add[l]!=[]:
+                
+                if safe_insertion:
+                                                                                                                    
+                    if hash_seg(segs_to_add[l]) not in self.H[l]:
+                    
+                        self.segments_set[l].append(segs_to_add[l])
+                                                
+                        self.H[l].append(hash_seg(segs_to_add[l]))
+                            
+                else:
+                        
+                    self.segments_set[l].append(segs_to_add[l])
+                                            
+                    self.H[l].append(hash_seg(segs_to_add[l]))
+                    
+    """ NOT FULLY IMPLEMENTED
+            
+    def select_var_to_branch(self): #return a tuple (row, leaf)
+        
+        global VARS
+    
+        best_row, best_leaf, best_score = None, None, 0
+        
+        sol=self.prob.solution.get_values()
+        
+        for r in range(get_data_size()):
+            
+            for l in range(len(self.segments_set)):
+                
+                score=0
+                
+                for s in self.segments_set[l]:
+                    
+                    if r in s:
+                
+                        score = score + sol[VARS["segment_leaf_"+str(s)+"_"+str(l)]]
+                        
+                if score > best_score and score < 0.99:
+                    
+                    best_row, best_leaf, best_score = r, l, score
+        
+        return best_row, best_leaf
+            
+    def create_children_by_branching(self,row,leaf):
+        
+        child1_segments = adapt_segments_set(self.segments_set,row,leaf,1)
+        
+        child1_prob = construct_master_problem(child1_segments,depth)
+        
+        self.child1 = BaP_Node(child1_segments,child1_prob,self.ID.append(1),self,self.branched_rows.append(row),self.branched_leaves.append(leaf))
+        
+        child0_segments = adapt_segments_set(self.segments_set,row,leaf,0)
+        
+        child0_prob = construct_master_problem(child0_segments,depth)
+        
+        self.child0 = BaP_Node(child0_segments,child0_prob,self.ID.append(0),self,self.branched_rows.append(row),self.branched_leaves.append(leaf))
+        
+        return
+
+    def solve_branch_and_price(self): #solve the branch and price problem recursively
+        
+        global best_solution
+        global best_solution_value
+        
+        self.solve_relaxation()
+                
+        if self.solution_type=='integer': 
+            
+            if self.solution_value < best_solution_value:
+            
+                best_solution=self.solution
+                best_solution_value=self.solution_value
+            
+            return self.solution_value
+        
+        elif self.solution_type=='infeasible':
+            
+            return float('+inf')
+        
+        else:
+            
+            if self.solution_value > best_solution_value:
+                
+                return self.solution_value
+            
+            else:
+                
+                row, leaf = self.select_var_to_branch()
+                
+                self.create_children_by_branching(row,leaf)
+                
+                return min(self.child1.solve_branch_and_price(), self.child2.solve_branch_and_price())
+                
+    """
---
+++ BaP_Solver.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 18 09:25:51 2018
+
+@author: mfirat
+"""
+
+class BaP_Solver:
+    
+     def __init__(self):
+ 
